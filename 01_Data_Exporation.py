3386c38c5e55f04dcbe0cdb5188a0d1f8e4b935d
MinPinSunHwa <chreey3098@naver.com>
2018-04-20 13:43:00 +0900

    ad

--- 01_Data_Exporation.py
+++ 01_Data_Exporation.py
@@ -16,56 +16,56 @@ print(ad.describe())
 
 
 ## Check the freqency of 'is_attributed' variable in ad
-from collections import Counter
-print(Counter(ad['is_attributed']))
+freq = ad['is_attributed'].value_counts(sort=False)
+print(freq)
+
+
+## Make derived values : click_hour
+ad['click_hour'] = np.nan
+ad['click_hour'] = ad['click_time'].dt.hour
+print(ad[['click_time','click_hour']].head(10))
+
+
+## Remove variable
+ad.drop('click_time', axis=1)
+ad.drop('attributed_time', axis=1)
 
 
 ## Make derived variable
 ## 'v'_cnt : frequency by 'v'
 ## 'v'_attr : the number of download by 'v'
 ## 'v'_attr_prop : the proporation of download by 'v'
-v = ['ip','app','device','os','channel']
-v1 = ['ip_cnt','app_cnt','device_cnt','os_cnt','channel_cnt']
-v2 = ['ip_attr','app_attr','device_attr','os_attr','channel_attr']
-v3 = ['ip_attr_prop','app_attr_prop','device_attr_prop','os_attr_prop','channel_attr_prop']
+## tot_prop : ip_attr_prop + app_attr_prop + device_prop + os_attr_prop + channel_attr_prop + hour_attr_prop
+v = ['ip','app','device','os','channel','click_hour']
+v1 = ['ip_cnt','app_cnt','device_cnt','os_cnt','channel_cnt','hour_cnt']
+v2 = ['ip_attr','app_attr','device_attr','os_attr','channel_attr','hour_attr']
+v3 = ['ip_attr_prop','app_attr_prop','device_attr_prop','os_attr_prop','channel_attr_prop','hour_attr_prop']
 
 for d,d1,d2,d3 in zip(v,v1,v2,v3):
     ad[d1] = np.nan
     ad[d1].fillna(ad.groupby(d)[d].transform('count'), inplace=True)
-    print(ad[d1].head(20))
+    print(ad[[d,d1]].head(20))
 
     ad[d2] = np.nan
     ad[d2].fillna(ad.groupby(d)['is_attributed'].transform('sum'), inplace=True)
-    print(ad[d2].head(20))
+    print(ad[[d,d2]].head(20))
 
     ad[d3] = np.nan
     ad[d3] = ad[d2] / ad[d1]
-    print(ad[d3].head(20))
+    print(ad[[d,d3]].head(20))
+        
+    ## Remove variable
+    ad.drop(d1, axis=1)
+    ad.drop(d2, axis=1)
 
-
-## Make derived values : click_hour
-ad['click_hour'] = np.nan
-hour = []
-for x in ad.click_time:
-    h = x.hour
-    hour.append(h)
-ad['click_hour'] = hour
-print(ad[['click_time','click_hour']].head(10))
+ad['tot_prop'] = np.nan
+ad['tot_prop'] = ad[v3].sum(axis=1)
+print(ad['tot_prop'].head(20))
 
 
 ## Check correlation
-print(ad[['ip_cnt', 'ip_attr', 'ip_attr_prop', 'app_cnt', 'app_attr', 'app_attr_prop', 
-          'device_cnt', 'device_attr', 'device_attr_prop', 'os_cnt', 'os_attr', 'os_attr_prop', 
-          'channel_cnt', 'channel_attr', 'channel_attr_prop', 'click_hour', 'is_attributed']].corr(method='pearson'))
-
-
-
-## Remove variable
-for i,j in zip(v1,v2):
-    del ad[i]
-    del ad[j]
-del ad['click_time']
-del ad['attributed_time']
+print(ad[['ip_attr_prop', 'app_attr_prop', 'device_attr_prop', 'os_attr_prop', 
+          'channel_attr_prop', 'hour_attr_prop', 'tot_prop', 'is_attributed']].corr(method='pearson'))
 
     
 ## Save dataset
--- 02_Preprocessing.py
+++ 02_Preprocessing.py
@@ -1,5 +1,5 @@
 
-## 2. Data Analysis
+## 2. Preprocessing
 ## Import library and data
 import pandas as pd
 import numpy as np
@@ -11,34 +11,37 @@ ad_test = ad = pd.read_csv('test.csv', parse_dates=['click_time'])
 print(ad_test.columns)
 
 
-##
-feat1 = ['ip','app','device','os','channel']
-feat2 = ['ip_attr_prop','ip_attr_prop','ip_attr_prop','ip_attr_prop','ip_attr_prop']
+## Make derived variable of test data : Click_hour
+ad_test['click_hour'] = np.nan
+ad_test['click_hour'] = ad_test['click_time'].dt.hour
+print(ad_test[['click_time','click_hour']].head(10))
+
+
+## Make derived variable of test data
+## 'v'_attr_prop : the proporation of download by 'v'
+## tot_prop : ip_attr_prop + app_attr_prop + device_prop + os_attr_prop + channel_attr_prop + hour_attr_prop
+feat1 = ['ip','app','device','os','channel','click_hour']
+feat2 = ['ip_attr_prop','app_attr_prop','device_attr_prop','os_attr_prop','channel_attr_prop','hour_attr_prop']
 
 for f1, f2 in zip(feat1,feat2):
     ad_test[f2] = np.nan
     a = ad.groupby(f1)[f2].mean()
 
     for x,y in zip(a.index,a):
-        ad_test[f2].loc[ad_test[f1] == x] = y
+        ad_test.ix[ad_test[f1] == x, f2] = y
     print(ad_test[[f1,f2]].head(20))
 
+
+ad_test['tot_prop'] = np.nan
+ad_test['tot_prop'] = ad_test[feat2].sum(axis=1)
+print(ad['tot_prop'].head(20))
 
 ##
 ad_test.fillna(0, inplace=True)
 
 
-##
-ad_test['click_hour'] = np.nan
-hour = []
-for x in ad_test.click_time:
-    h = x.hour
-    hour.append(h)
-ad_test['click_hour'] = hour
-print(ad_test[['click_time','click_hour']].head(10))
-
-
 ## Remove variable
+del ad_test['click_id']
 del ad_test['click_time']
 
 
