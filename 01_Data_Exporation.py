4f73311116290b9fea59b3e5027c2cd0366f185e
MinPinSunHwa <37681149+MinPinSunHwa@users.noreply.github.com>
2018-05-15 02:23:11 +0900

    update

--- 01_Data_Exporation.py
+++ 01_Data_Exporation.py
@@ -6,7 +6,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-ad = pd.read_csv("train_sample.csv", parse_dates=['click_time'])
+ad = pd.read_csv("train.csv", parse_dates=['click_time'])
 
 
 ## Create functions
@@ -14,18 +14,42 @@ def barplot(x, y, data):
     plt.figure(figsize=(15,15))
 
     plt.subplot(3,1,1)
-    plt.title('the frequency of ' + x)
+    plt.title('click count by ' + x)
     sns.countplot(x, data=data)
     
     plt.subplot(3,1,2)
-    plt.title('the number of download by ' + x)
+    plt.title('download count by ' + x)
     sns.barplot(x, y, data=data, estimator=sum, ci=None)
     
     plt.subplot(3,1,3)
-    plt.title('the proporation of download by ' + x)
+    plt.title('download rate by ' + x)
     sns.barplot(x, y, data=data, ci=None)
     
+    plt.savefig('graph/barplot_' + x + '.png')
     plt.show()
+    plt.close()
+    
+def barplot_r(x, y, data):
+    plt.figure(figsize=(25,20))
+
+    plt.subplot(3,1,1)
+    plt.title('click count by ' + x)
+    sns.countplot(x, data=data)
+    plt.xticks(rotation=90, fontsize="small")
+    
+    plt.subplot(3,1,2)
+    plt.title('download count by ' + x)
+    sns.barplot(x, y, data=data, estimator=sum, ci=None)
+    plt.xticks(rotation=90, fontsize="small")
+    
+    plt.subplot(3,1,3)
+    plt.title('download rate by ' + x)
+    sns.barplot(x, y, data=data, ci=None)
+    plt.xticks(rotation=90, fontsize="small")
+
+    plt.savefig('graph/barplot_' + x + '.png')  
+    plt.show()
+    plt.close()
 
 
 ## Check dataset
@@ -41,51 +65,45 @@ freq = ad['is_attributed'].value_counts(sort=False)
 print(freq)
 
 
-## Check the number of download over time
+## Check the number of downloads over time
 temp = ad['is_attributed']
 temp.index = ad['click_time']
 temp = temp.resample('H').sum()
 
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10,5))
 plt.title('The number of download by click time')
 plt.plot(temp.index, temp)
+plt.savefig('graph/timeplot.png')
 plt.show()
 
 
-## Check download per hour
+## Check click count, download count, download rate per hour
 ad['hour'] = np.nan
 ad['hour'] = ad['click_time'].dt.hour
 barplot('hour', 'is_attributed', ad)
 
 
 ## Draw barplots
-barplot('ip', 'is_attributed', ad)
-barplot('app', 'is_attributed', ad)
-barplot('device', 'is_attributed', ad)
-barplot('os', 'is_attributed', ad)
-barplot('channel', 'is_attributed', ad)
-
-
-freq = ad.ip.value_counts(sort=True)
-freq[:30]
+barplot_r('app', 'is_attributed', ad)
+barplot_r('device', 'is_attributed', ad)
+barplot_r('os', 'is_attributed', ad)
+barplot_r('channel', 'is_attributed', ad)
 
 
 ## Check correlation
-feat = ['ip','app','device','os','channel']
-
-pd.plotting.scatter_matrix(ad[feat], figsize=(15,15), alpha=.1)
-
+feat = ['ip','app','device','os','channel','hour','is_attributed']
 print(ad[feat].corr(method='pearson'))
-print(ad[feat].corr(method='spearman'))
+pd.plotting.scatter_matrix(ad[feat], figsize=(15,15), alpha=.1)
+plt.savefig('graph/scatterplot.png')
+plt.show()
+
 
 
 ## Set options
-pd.set_option('display.max_columns', 200)
-
-
+# pd.set_option('display.max_columns', 200)
 
 ## countplot
 ## https://seaborn.pydata.org/generated/seaborn.countplot.html
 
-## varplot
+## barplot
 ## https://seaborn.pydata.org/generated/seaborn.barplot.html
