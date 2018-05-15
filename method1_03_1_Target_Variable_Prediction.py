46014c6620cf7e1b356a01684e8626c86df1407c
MinPinSunHwa <37681149+MinPinSunHwa@users.noreply.github.com>
2018-05-16 01:24:10 +0900

    update

--- method1_03_1_Target_Variable_Prediction.py
+++ method1_03_1_Target_Variable_Prediction.py
@@ -87,13 +87,11 @@ feat3 = feat1 + feat2
 feat4 = ['ip_attr_prop','app_attr_prop','channel_attr_prop','tot_attr_prop']
 feat5 = feat4 + feat2
 feat6 = ['app_attr_prop','channel_attr_prop','hour_app_prop','hour_channel_prop']
-feat7 = ['app_attr_prop','device_attr_prop','os_attr_prop','channel_attr_prop','hour_attr_prop']
-feat8 = feat7 + ['hour_app_prop','hour_channel_prop']
 
 
 ## Predict a target variable
-feat = [feat1,feat2,feat3,feat4,feat5,feat6,feat7,feat8]
-name = ['feat1','feat2','feat3','feat4','feat5','feat6','feat7','feat8']
+feat = [feat1,feat2,feat3,feat4,feat5,feat6]
+name = ['feat1','feat2','feat3','feat4','feat5','feat6']
 # sample = '10m_'
 # sample = '20m_'
 # sample = '30m_'
