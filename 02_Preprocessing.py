9bf0ce5df0f0946b718d0aa1f6cc8917c6cd12fc
MinPinSunHwa <37681149+MinPinSunHwa@users.noreply.github.com>
2018-07-30 06:15:24 +0900

    update

--- 02_Preprocessing.py
+++ 02_Preprocessing.py
@@ -3,29 +3,90 @@
 ## Import library
 import pandas as pd
 import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 import gc
 
 
+## Load data
+data = pd.read_csv("merge.csv", parse_dates=['click_time'])
+ip = pd.read_csv("blacklist/ip_download.csv")
+app = pd.read_csv("blacklist/app_download.csv")
+device = pd.read_csv("blacklist/device_download.csv")
+os = pd.read_csv("blacklist/os_download.csv")
+gc.collect()
 
-# Merge train data and test data
-del train['attributed_time']
-
-test['is_attributed'] = 0
+print(data.head())
+print(data.tail())
 
 
+##
+def merge_black(df, black, feat):
+    temp = black[[feat, 'gap','black_'+feat]]
+    df = df.merge(temp, on=feat, how='left')
+    df.rename(columns={'gap':'gap_'+feat}, inplace = True)
+    gc.collect()
+    
+    print(df.head())
+    return df
+    
+data = merge_black(data, ip, 'ip')
+data = merge_black(data, app, 'app')
+data = merge_black(data, device, 'device')
+data = merge_black(data, os, 'os')
+
+
+##
+feat = ['is_attributed', 'gap_ip', 'black_ip']
+plt.figure(figsize=(20,20))
+sns.pairplot(data[feat].loc[data['click_id'].isnull()], hue='is_attributed', palette="husl", alpha=.2)
+plt.savefig('graph/scatter_plot_gap_black_ip.png')
+plt.show()
+gc.collect()
+
+
+##
+feat = ['is_attributed', 'gap_app', 'black_app']
+plt.figure(figsize=(20,20))
+sns.pairplot(data[feat].loc[data['click_id'].isnull()], hue='is_attributed', palette="husl", alpha=.2)
+plt.savefig('graph/scatter_plot_gap_black_app.png')
+plt.show()
+gc.collect()
+    
+    
+##
+feat = ['is_attributed', 'gap_ip', 'black_ip', 'gap_app', 'black_app', 'gap_device', 'black_device', 'gap_os', 'black_os']
+plt.figure(figsize=(20,20))
+sns.pairplot(data[feat].loc[data['click_id'].isnull()], palette="husl", alpha=.2)
+plt.savefig('graph/scatter_plot_gap_black.png')
+plt.show()
+gc.collect()    
+    
 
 
 # Make a derived variable : click_gap
-
+train = pd.read_csv('train.csv')
 for ip in train.ip.value_counts().index:
-    temp = train
+    temp = train.loc[train['ip'] == ip, 'click_time']
+    temp.sort_values(ascending=False, inplace=True)
+    
+    temp['click_gap'] = np.nan
+    temp[1:].loc['click_gap'] = temp[:-1].loc['click_time']
+    temp['click_gap'] = temp['click_gap'] - temp['click_time']
+
+temp = train.loc[train['ip'] == 10, 'click_time'].reset_index()
+temp.sort_values(ascending=True, by='click_time', inplace=True)
+
+temp['click_time'] = np.nan
+temp[1:].loc['click_gap'] = temp[:-1].loc['click_time']
+temp['click_gap'] = temp['click_gap'] - temp['click_time']
+    
+    
 a = train['click_time'].iloc[1] - train['click_time'].iloc[0]
 
 
 
 
 
-## Check click count, download count, download rate per hour
-ad['hour'] = np.nan
-ad['hour'] = ad['click_time'].dt.hour
-barplot('hour', 'is_attributed', ad)
